8e777d75f2494c54304345faaec72bbfd4a90e73
Arty <gizzmaster@gmail.com>
2023-05-16 14:33:28 +0300

    update temp algotithm for r pdd, update templates

--- app/temporary/pars_pdd_temp.py
+++ app/temporary/pars_pdd_temp.py
@@ -1,6 +1,4 @@
-from collections import defaultdict
 import sys
-from typing import Any
 import pyqtgraph as pg
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import json as js
@@ -9,43 +7,6 @@ from functools import lru_cache
 from time import perf_counter_ns
 import struct
 import numpy as np
-import pickle
-
-
-def load_json(filepath: str) -> Any:
-    with open(filepath, 'r', encoding='utf8') as file:
-        return js.load(file)
-
-
-def graph(x, y):
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-    plot_widget = pg.PlotWidget(title="freq", background='white')
-    plot_widget.plot(x, y, pen='r')
-    win.setCentralWidget(plot_widget)
-    win.show()
-    sys.exit(app.exec_())
-
-
-def get_mask(fields):
-    structure = list(range(0, 32, 2))
-    for elem in fields:
-        type = elem['type']
-        offset = elem['offset']
-        if type == 'int':
-            mask = 'h'
-        elif type == 'uint':
-            mask = 'H'
-        elif type == 'pre':
-            mask = 'H'
-            # mask += '2s'
-        else:
-            raise ValueError('not find type')
-        index = structure.index(offset)
-        structure[index] = mask
-    mask = '>' + ''.join(['xx' if isinstance(i, int)
-                         else i for i in structure])
-    return mask
 
 
 def get_mask_shift_from_field(size):
@@ -55,141 +16,135 @@ def get_mask_shift_from_field(size):
     shift = start
     return number_mask, shift
 
-def unpack_groups(json_groups, result_df):
 
-    for group in json_groups:
-        masks_shifts = np.array([get_mask_shift_from_field(
-            field['size']) for field in group['fields']])
-        numbers = result_df[group['name']].values
-        result_list = np.zeros(
-            (len(numbers), len(masks_shifts)), dtype=np.int64)
-        for i, (number_mask, shift) in enumerate(masks_shifts):
-            result_list[:, i] = (numbers & (number_mask << shift)) >> shift
-        columns = [field['name'] for field in group['fields']]
-        new_df = pd.DataFrame(result_list, columns=columns)
+def load_json(filepath: str):
+    with open(filepath, 'r', encoding='utf8') as file:
+        return js.load(file)
 
-        result_df = pd.concat(
-            [result_df, new_df], axis=1)
-    return result_df
 
-def unpack_string(byte_str: bytes, adr: dict, result_data: dict):
-    # контрольная сумма всегда в 4-5 байте (5-6)
-    if adr['ks'] != byte_str[4:6]:
-        return
+def graph(df: pd.DataFrame, column):
+
+    filtered_df = df.dropna(subset=['time', column]).reset_index()
+
+    x = filtered_df.time.to_list()
+    y = filtered_df[column].to_list()
+
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+
+    plot_widget = pg.PlotWidget(title="freq", background='white')
+    plot_widget.plot(x, y, pen='r')
+    win.setCentralWidget(plot_widget)
+    win.show()
+    sys.exit(app.exec_())
+
+
+def get_unpacked_data_list(filepath):
+    with open(filepath, 'rb') as file:
+        string = file.read()
+        unpacked_data_list = [
+            i for i in struct.iter_unpack('>IH4x16H', string)]
+        unpacked_data_list = np.array(unpacked_data_list)
+    return unpacked_data_list
+
+
+def unpack_element(byteswap, field_data, data_source: np.array):
+    position = field_data['position'] + 2
+    koef = field_data['koef']
+    size = field_data['size']
+    type = field_data['type']
+    mask, shift = get_mask_shift_from_field(size)
+    data_column = data_source[:, position]
+    if type == 'uint16' or type == 'pre':
+        type = np.uint16
+    elif type == 'uint8':
+        type = np.uint8
+    elif type == 'int16':
+        type = np.int16
+    elif type == 'int8':
+        type = np.int8
+    data_column = data_column.astype(type)
+    if not byteswap:
+        data_column = data_column.byteswap()
+    masked_data = np.bitwise_and(data_column, (mask << shift))
+    masked_data = np.right_shift(masked_data, shift) * koef
+    return data_column
+
+
+def unpack_group(byteswap, group_info, data_source, result_data):
+    if type(group_info['fields']) == list:
+        for field in group_info['fields']:
+            column_name = field['name']
+            column = unpack_element(byteswap, field, data_source)
+            result_data[column_name] = column
     else:
-        # время всегда в первых 4 байтах (0-3)
-        time = struct.unpack('<I', byte_str[:4])[0]
-        # данные всегда начинаются с 11 байта (10-42)
-        data_byte = byte_str[10:]
-        nums = struct.unpack(adr['group_mask'], data_byte)
-        result_data['main'].append((time, *nums))
-        if 'additionals' in adr.keys():
-            for additional in adr['additionals']:
-                condition = (data_byte[additional['offset']] & (
-                    additional['number_mask'] << additional['shift']
-                    )) >> additional['shift']
-
-                if condition == additional['condition']:
-                    nums = struct.unpack(additional['group_mask'], data_byte)
-                    result_data['additionals'][additional['condition']].append((time, *nums))
+        mask_condition, shift_condition = get_mask_shift_from_field(
+            group_info['condition_bit'])
+        condition_data = (data_source[:, group_info['condition_byte'] + 2]
+                          & (mask_condition << shift_condition)) >> shift_condition
+        for condition, fields in group_info['fields'].items():
+            condition_mask = condition_data == int(condition)
+            for field in fields:
+                column_name = field['name']
+                column = unpack_element(byteswap, field, data_source)
+                column = np.where(condition_mask, column, np.nan)
+                result_data[column_name] = column
 
 
-def load_pdd(pddfilepath: str, json_filepath: str) -> dict:
-    # загружаем описатель из json
-    j_data = load_json(json_filepath)
-    # приводим данные в json в порядок
-    for adr in j_data['adr']:
-        adr['fields'].sort(key=lambda x: x['offset'])
-        adr['group_mask'] = get_mask(adr['fields'])
-        adr['ks'] = int(adr['ks'], base=16).to_bytes(
-            2, byteorder='little', signed=False)
-        if 'additionals' in adr.keys():
-            for additional in adr['additionals']:
-                additional['fields'].sort(key=lambda x: x['offset'])
-                additional['group_mask'] = get_mask(additional['fields'])
-                additional['number_mask'], additional['shift'] = get_mask_shift_from_field(
-                    additional['condition_address'])
-    # начинаем формировать словарь, результатов
-    result = {}
-    result['name'] = j_data['name']
-    # формируем разметку результирующего словаря
-    for adr in j_data['adr']:
-        result['adr'] = {adr['adr_name']: {'main': []}}
-        if 'additionals' in adr.keys():
-            result['adr'][adr['adr_name']]['additionals'] = []
-            for _ in adr['additionals']:
-                result['adr'][adr['adr_name']]['additionals'].append([])
-    # result['adr'] = {
-    #     adr['adr_name']: {'main':[]} for adr in j_data['adr']
-    # }
-
-    #читаем файл и распаковываем строки
-    with open(pddfilepath, 'rb') as file:
-        string = file.read(42)
-        while len(string) == 42:
-            for elem in j_data['adr']:
-                unpack_string(
-                    string, elem, result['adr'][elem['adr_name']]
-                )
-            string = file.read(42)
-
-    print(perf_counter_ns() - a, 'файл прочитан')
-
-    #формируем основной датафрейм и добавляем к нему группы условий
-    for adr in j_data['adr']:
-        columns = ['time'] + [field['name'] for field in adr['fields']]
-        rows = np.array(result['adr'][adr['adr_name']]['main'])
-        # result['adr'][adr['adr_name']] = pd.DataFrame(
-        #     data=rows, columns=columns, dtype=int)
-        main_df = pd.DataFrame(
-            data=rows, columns=columns, dtype=int)
-        for additional in adr['additionals']:
-            columns = ['time'] + [field['name'] for field in additional['fields']]
-            rows = np.array(result['adr'][adr['adr_name']]['additionals'][additional['condition']])
-            df_additional = pd.DataFrame(data=rows, columns=columns, dtype=int)
-            main_df = pd.merge(main_df, df_additional, on='time', how='left')
-
-        #добавить умножение коэффициенто
-        main_df.time = main_df.time * 0.0001
-
-        result['adr'][adr['adr_name']] = main_df
+def get_filtered_data_by_checksum(checksum, source_data):
+    control_sum = source_data[:, 1].astype(np.uint16).byteswap()
+    control_sum_mask = control_sum == checksum
+    data_list = source_data[control_sum_mask]
+    return data_list
 
 
-
-    print(perf_counter_ns() - a, 'дф сформированы')
-
-    #распаковываем группы
-    for adr in j_data['adr']:
-        result['adr'][adr['adr_name']] = unpack_groups(
-            adr['groups'], result['adr'][adr['adr_name']])
+def get_time_list(koef, source):
+    time_list = source[:, 0].astype(np.uint32)
+    time_list = time_list.byteswap() * koef
+    return time_list
 
 
-    print(perf_counter_ns() - a, 'группы обработаны')
-
-    # a = np.array(result['adr'][adr['adr_name']]['additionals'][1])
-    # graph(a[::,0], a[::,1])
-        
-
-    return result
-
-#решить вопрос с размером
-
-a = perf_counter_ns()
+def unpack_fields(byteswap, fields, data_list, result_dict):
+    for field in fields:
+        if 'group' in field.keys() and field['group'] == True:
+            unpack_group(byteswap, field, data_list, result_dict)
+        else:
+            column_name = field['name']
+            column = unpack_element(byteswap, field, data_list)
+            result_dict[column_name] = column
+    return result_dict
 
 
-result: pd.DataFrame = load_pdd(
-    r"C:\Users\ONT\Downloads\RES.pdd",
-    r'C:\Tikhonov\datanalysis\templates\default_d001_v1_11.json'
-)
+def unpack_adr(adr, unpacked_data_list):
+    checksum = int(adr['checksum'], base=16)
+    data_list = get_filtered_data_by_checksum(checksum, unpacked_data_list)
+    df_dict = {}
+    df_dict['time'] = get_time_list(adr['time_koef'], data_list)
+    df_dict = unpack_fields(
+        adr['bytes_swap'], adr['fields'], data_list, df_dict)
+    return df_dict
 
 
-# with open('temp/pdd_diss.pkl', 'wb') as f:
+def load_pdd(filepath_pdd, filepath_json):
+    json_data = load_json(filepath_json)
+    unpacked_data_list = get_unpacked_data_list(filepath_pdd)
+    result_dict = {}
+    for adr in json_data['adr']:
+        adr_data = unpack_adr(adr, unpacked_data_list)
+        result_dict[adr['adr_name']] = pd.DataFrame(adr_data)
+    return result_dict
 
-#     pickle.dump({'DISS01': result['adr']}, f, protocol=4)
 
-result = result['adr']['ADR1']
+start = perf_counter_ns()
+
+json_path = r'C:\Tikhonov\datanalysis\templates\d001.json'
+pdd_filepath = r"C:\Users\ONT\Downloads\RES (1).pdd"
 
 
-# print(result.group_otkaz)
-#print(result)
-graph(result.time, result.KorrUp_ch0)
+result = load_pdd(pdd_filepath, json_path)
+
+
+print(perf_counter_ns() - start, 'finish')
+
+# print(result)
+graph(result['ADR1'], 'Fd2')
